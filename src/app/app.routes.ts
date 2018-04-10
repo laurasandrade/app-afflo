e23c4378025eaf928cef957c0f1204daed8cfdd0
Laura Andrade <laura.andradeweb@gmail.com>
2018-04-10 12:58:02 -0300

    Finalizando rotas login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 
 const appRoutes: Routes  = [
-  { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
-   { path: '**', redirectTo: ''}
+	{ path: '', component: LoginComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'cadastro', component: CadastroComponent },
+	{ path: '**', redirectTo: ''},
+	{ path: '', redirectTo: '/cadastro', pathMatch: 'full' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
