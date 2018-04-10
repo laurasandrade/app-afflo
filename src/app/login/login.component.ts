305a6c8b5fb556421ff5d62d7363926d0a95c33c
Laura Andrade <laura.andradeweb@gmail.com>
2018-04-10 13:59:12 -0300

    alteração

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {Component} from '@angular/core';
+import {Http,Headers} from '@angular/http';
+import {FormGroup,FormBuilder,Validators} from '@angular/forms';
 
 
 @Component({
