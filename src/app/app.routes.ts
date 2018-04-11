d56167e6abc91126ee23074f3b20cfb6ef8ba969
puppe1990 <matheus.puppe90@hotmail.com>
2018-04-11 10:35:18 -0300

    estrutura componente html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { QuestionarioComponent } from './questionario/questionario.component';
+
 
 const appRoutes: Routes  = [
 	{ path: '', component: LoginComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'cadastro', component: CadastroComponent },
+	{ path: 'questionario', component: QuestionarioComponent },	
 	{ path: '**', redirectTo: ''},
 	{ path: '', redirectTo: '/cadastro', pathMatch: 'full' }
 ];
