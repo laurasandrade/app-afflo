f20f2da32cd7d25ab4f53645a3161ead942e04b6
Laura Andrade <laura.andradeweb@gmail.com>
2018-04-11 17:44:08 -0300

    Add login service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,33 +7,47 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { routing } from './app.routes';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from "angularfire2/auth";
 
-import {LoginService} from './login.service'
+import { LoginService } from './login.service';
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAFslbeTC63m9UH39I_G0ujnElbgLctKIQ',
-  authDomain: 'app-afflo.firebaseapp.com',
-  databaseURL: 'https://app-afflo.firebaseio.com',
-  storageBucket: 'app-afflo.appspot.com',
-  messagingSenderId: '281776171082'
+
+ apiKey: 'AIzaSyCkfcRqpbFF2xyAIWbie9wn2_cFQX5wIsA',
+ authDomain: 'angular-bf420.firebaseapp.com',
+ databaseURL: 'https://angular-bf420.firebaseio.com',
+ storageBucket: 'angular-bf420.appspot.com',
+ messagingSenderId: '229553449034'
+
 };
 
 
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    CadastroComponent
-    ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    routing,
-    FormsModule, 
-    ReactiveFormsModule,
-    AngularFireModule.initializeApp(firebaseConfig)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+ declarations: [
+   AppComponent,
+   LoginComponent,
+   CadastroComponent
+ ],
+
+ imports: [
+   BrowserModule,
+   HttpModule,
+   routing,
+   FormsModule,
+   ReactiveFormsModule,
+   AngularFireModule.initializeApp(firebaseConfig)
+ ],
+
+ providers: [
+   AngularFireModule,
+   AngularFireAuth,
+   LoginService
+ ],
+
+ bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -4,39 +4,53 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 
 
+
+
 @Injectable()
+
 export class LoginService {
 
-    user: Observable<firebase.User>;
+ user: Observable<firebase.User>;
 
-    constructor(public afAuth: AngularFireAuth) { 
-   this.user = afAuth.authState;
-}
 
-    public login(mail: string, password: string) {
+ constructor(public afAuth: AngularFireAuth) {
 
-        return new Promise((resolve, reject) => {
+   this.user = afAuth.authState
 
-            this.afAuth.auth.signInWithEmailAndPassword(mail, password).then((user) => {
+ }
 
-                localStorage['token'] = user.Yd;
-                this.router.navigate(['']);
 
-            })
-                .catch((error) => {
-                    console.log(error);
-                    this.router.navigate(['/login']);
-                });
-        })
-            .catch((error) => {
-                console.log(error);
-                this.router.navigate(['/login']);
-            });
-    }
 
-    public logout() {
-        return this.afAuth.auth.signOut();
-    }
+ public login(mail: string, password: string) {
 
+   return new Promise((resolve, reject) => {
+
+     this.afAuth.auth.signInWithEmailAndPassword(mail, password).then((user) => {
+
+       localStorage['token'] = user.uid;
+
+     })
+
+     .catch((error) => {
+
+       // this.router.navigate(['/login']);
+
+     });
+
+   })
+
+   .catch((error) => {
+
+   });
+
+ }
+
+
+
+ public logout() {
+
+   return this.afAuth.auth.signOut();
+
+ }
 
 }
